d03f8e20b5344dae90f1e736b81fe5d5087a896a
CShark100 <3937147@gmail.com>
2024-08-02 00:09:11 +0300

    Add files via upload

---
+++ module_1_3.py
@@ -0,0 +1,9 @@
+# Динамическая типизация
+name = 'Serega'
+print('Name: ' + name)
+age = 44
+print('Age:', age)
+age = age + 1           # или можно создать новую переменную и прибавить её к переменной age
+print('Age:', age)
+is_student = 'True'
+print('Is Student:', is_student)
