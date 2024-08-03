2c06e83d2f215e1ee99ebd1ba661f5369ecfe0e2
CShark100 <3937147@gmail.com>
2024-08-04 00:36:42 +0300

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,8 @@
+# Организация программ и методы строк
+my_string = input("Введите произвольный текст: ")
+print("Введено символов: ", my_string.count("")-1)      # считаем кол-во введенных символов
+print(my_string .upper())                               # верхний регистр
+print(my_string .lower())                               # нижний регистр
+print(my_string .replace(' ', ''))          # удаляем пробелы
+print(my_string [0])                                    # первый символ
+print(my_string [-1])                                   # последний символ
